d645e1bdbd13959bcdf897e4049bb4f6b9b1fb94
Devin Alexander <devinmalexander@gmail.com>
2025-01-15 07:48:54 -0600

    Level Data Setup

--- singletons/level_data.gd
+++ singletons/level_data.gd
@@ -11,10 +11,23 @@ func _ready() -> void:
 	load_level_data()
 	pass
 
+func setup_level(lns: String, raw_level_data: Dictionary) -> LevelLayout:
+	var layout: LevelLayout = LevelLayout.new()
+	
+	var ps = raw_level_data.player_start
+	layout.set_player_start(ps.x, ps.y)
+	
+	return layout
 
 func load_level_data() -> void:
 	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
 	var raw_data = JSON.parse_string(file.get_as_text())
 
 	for lns in raw_data.keys():
-		_level_data[lns] = lns + "_val"
+		var new_level_layout: LevelLayout = setup_level(lns, raw_data[lns])
+#		print("new_level_layout:%s %d,%d" % [
+#			new_level_layout.get_instance_id(),
+#			new_level_layout.get_player_start().x,
+#			new_level_layout.get_player_start().y
+#		])
+		_level_data[lns] = new_level_layout
